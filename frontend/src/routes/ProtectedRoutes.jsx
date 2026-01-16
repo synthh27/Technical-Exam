a88bea9a90fd67716b4a31352e9f7e94cee29cc5
synthh <sobrepenacarliannoel@gmail.com>
2026-01-16 22:07:19 +0800

    ADDED Import Navigate rrd

--- frontend/src/routes/ProtectedRoutes.jsx
+++ frontend/src/routes/ProtectedRoutes.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoutes = ({ children }) => {
     
