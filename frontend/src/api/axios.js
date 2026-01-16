66360f074e374f6609917667ef74cddb7dffe2a0
synthh <sobrepenacarliannoel@gmail.com>
2026-01-16 18:42:34 +0800

    ADDED Interceptors to automatically add JWT Token if it exist

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,6 +2,21 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
+// INTERCEPTORS
+
+api.interceptors.request.use((config => {
+  // GET TOKEN FROM LOCAL STORAGE
+  const token = localStorage.getItem('token');
+
+  // IF TOKEN EXISTS, ADD IT TO REQUEST HEADERS AUTOMATICALLY
+  if (token) config.headers['Authorization'] = `Bearer ${token}`;
+  return config;
+}, (error) => { return Promise.reject(error);}));
+
 export default api;
+  
