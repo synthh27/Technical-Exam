563ec71bba4eec25a6d4df487993e8983e6443a4
synthh <sobrepenacarliannoel@gmail.com>
2026-01-16 22:09:13 +0800

    ADDED Undefined value for baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:7144/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
+  withCredentials: true, // optional, only if using cookies
 });
 
 // INTERCEPTORS
-
 api.interceptors.request.use((config => {
   // GET TOKEN FROM LOCAL STORAGE
   const token = localStorage.getItem('token');
