757930bf319af056d6a618263f5fc9116e53ac92
synthh <sobrepenacarliannoel@gmail.com>
2026-01-16 19:12:51 +0800

    UPDATED App.jsx and ADDED AuthProvider and Protected routes to block unauthorized users' access

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import { AuthProvider } from './context/AuthProvider.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css'
-import Tasks from "./Tasks"
+import Register from './pages/Register.jsx';
+import Tasks from "./pages/Tasks.jsx"
+import Login from './pages/Login.jsx';
 
 function App() {
   return (
-    <div className="app">
-      <h1>📝 React Task Evaluator</h1>
-      <Tasks />
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/tasks" element={
+            <ProtectedRoute>
+              <Tasks />
+            </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
