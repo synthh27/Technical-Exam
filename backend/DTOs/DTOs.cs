7bd299b7544bccec7b76b0a0aabfad84f22d190f
synthh <sobrepenacarliannoel@gmail.com>
2026-01-16 12:55:15 +0800

    CREATED Auth DTOs

---
+++ backend/DTOs/DTOs.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManager.DTOs
+{
+    public class DTOs
+    {
+        public record AuthRequest
+        (
+            string Enail,
+            string Password
+        );
+
+        public record AuthResponse
+        (
+            int userId,
+            string Email,       
+            string Token
+        );
+    }
+}
