2109614a9d0a3b4e5504947cf3ff5e71df31f314
synthh <sobrepenacarliannoel@gmail.com>
2026-01-16 12:34:41 +0800

    CREATED AuthController.cs

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.API;
+using TaskManager.Data;
+
+namespace task_manager_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
