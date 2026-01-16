072250c773816e62f9c419e645d9da1383f45613
synthh <sobrepenacarliannoel@gmail.com>
2026-01-16 15:11:12 +0800

    CREATED A base contrller and ADDED Parsed UserId from the JWT Token

---
+++ backend/Controllers/Base/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace TaskManager.Controllers.Base
+{
+    public class BaseController : ControllerBase
+    {
+        protected int UserId => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+    }
+}
