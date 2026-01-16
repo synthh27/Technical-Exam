a8e67d396d3dc4b8edd76e21767f4057724192d4
synthh <sobrepenacarliannoel@gmail.com>
2026-01-16 13:02:00 +0800

    CREATED JWTService Services

---
+++ backend/Services/JwtService.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.Services
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+    }
+}
